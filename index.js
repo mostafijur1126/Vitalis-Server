e7cc8e1b67bd5d848ae724c916304b6cc826fa72
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-06-20 19:57:09 +0600

    fix essue

--- index.js
+++ index.js
@@ -24,138 +24,156 @@ const client = new MongoClient(uri, {
   },
 });
 
-async function run() {
+// async function run() {
+//   try {
+
+// Connect the client to the server	(optional starting in v4.7)
+// await client.connect();
+
+client
+  .connect(() => {
+    console.log("connecting to Mongo db");
+  })
+  .catch(console.dir);
+
+const db = client.db("vitals");
+const classCollection = db.collection("allClasses");
+const subscriptionsCollection = db.collection("subscriptions");
+const bookClassCollection = db.collection("bookClasses");
+const favoriteCollection = db.collection("favorite");
+const forumPostCollection = db.collection("forumPost");
+const userCollection = db.collection("user");
+
+app.get("/api/all-class", async (req, res) => {
   try {
-    const db = client.db("vitals");
-    const classCollection = db.collection("allClasses");
-    const subscriptionsCollection = db.collection("subscriptions");
-    const bookClassCollection = db.collection("bookClasses");
-    const favoriteCollection = db.collection("favorite");
-    const userCollection = db.collection("user");
+    const { search = "", category = "" } = req.query;
 
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    const query = {};
 
-    app.get("/api/all-class", async (req, res) => {
-      try {
-        const { search = "", category = "" } = req.query;
-
-        const query = {};
-
-        // Search by className
-        if (search) {
-          query.className = {
-            $regex: search,
-            $options: "i",
-          };
-        }
-
-        // Category filter
-        if (category && category !== "All Categories") {
-          query.category = category;
-        }
-
-        const result = await classCollection.find(query).toArray();
-
-        res.send(result);
-      } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: "Server Error" });
-      }
-    });
-
-    app.get("/api/all-classes/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = {
-        _id: new ObjectId(id),
+    // Search by className
+    if (search) {
+      query.className = {
+        $regex: search,
+        $options: "i",
       };
-      const resust = await classCollection.findOne(query);
-      res.send(resust || {});
-    });
+    }
 
-    app.post("/api/add-class", async (req, res) => {
-      const data = req.body;
-      const resust = await classCollection.insertOne(data);
-      res.send(resust);
-    });
+    // Category filter
+    if (category && category !== "All Categories") {
+      query.category = category;
+    }
 
-    app.post("/api/subscription", async (req, res) => {
-      const { sessionId, userId, priceId } = req.body;
+    const result = await classCollection.find(query).toArray();
 
-      const isExist = await subscriptionsCollection.findOne({ sessionId });
-      if (isExist) {
-        return res.json({ msg: "Already exist!" });
-      }
-      await subscriptionsCollection.insertOne({
-        sessionId,
-        userId,
-        priceId,
-      });
-
-      //update userPlan
-      await userCollection.updateOne(
-        { _id: new ObjectId(userId) },
-        { $set: { plan: "pro" } },
-      );
-
-      res.json({ msg: "Payment Successfuly!" });
-    });
-
-    //class booking related
-    app.get("/api/checkBooking", async (req, res) => {
-      const { userId, classId } = req.query;
-      const existing = await bookClassCollection.findOne({ userId, classId });
-      res.status(200).json({ isBooked: !!existing });
-    });
-    app.post("/api/bookClass", async (req, res) => {
-      const result = await bookClassCollection.insertOne(req.body);
-      res.status(200).json(result);
-    });
-
-    //favorite add/remove
-    app.post("/api/favorites", async (req, res) => {
-      const { userId, classId } = req.body;
-      const existing = await favoriteCollection.findOne({ userId, classId });
-      if (existing) {
-        await favoriteCollection.deleteOne({ userId, classId });
-        res
-          .status(200)
-          .json({ isFavorite: false, message: "Removed from favorites" });
-      } else {
-        await favoriteCollection.insertOne({
-          ...req.body,
-          createdAt: new Date(),
-        });
-        res
-          .status(200)
-          .json({ isFavorite: true, message: "Added to favorites" });
-      }
-    });
-
-    app.get("/api/favorites/check", async (req, res) => {
-      const { userId, classId } = req.query;
-      const existing = await favoriteCollection.findOne({ userId, classId });
-      res.status(200).json({ isFavorite: !!existing });
-    });
-
-    // User এর সব favorites
-    // app.get("/api/favorites", async (req, res) => {
-    //   const { userId } = req.query;
-    //   const favorites = await favoritesCollection.find({ userId }).toArray();
-    //   res.status(200).json(favorites);
-    // });
-
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!",
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Server Error" });
   }
-}
-run().catch(console.dir);
+});
+
+app.get("/api/all-classes/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = {
+    _id: new ObjectId(id),
+  };
+  const resust = await classCollection.findOne(query);
+  res.send(resust || {});
+});
+
+app.post("/api/add-class", async (req, res) => {
+  const data = req.body;
+  const resust = await classCollection.insertOne(data);
+  res.send(resust);
+});
+
+app.post("/api/subscription", async (req, res) => {
+  const { sessionId, userId, priceId } = req.body;
+
+  const isExist = await subscriptionsCollection.findOne({ sessionId });
+  if (isExist) {
+    return res.json({ msg: "Already exist!" });
+  }
+  await subscriptionsCollection.insertOne({
+    sessionId,
+    userId,
+    priceId,
+  });
+
+  //update userPlan
+  await userCollection.updateOne(
+    { _id: new ObjectId(userId) },
+    { $set: { plan: "pro" } },
+  );
+
+  res.json({ msg: "Payment Successfuly!" });
+});
+
+//class booking related
+app.get("/api/checkBooking", async (req, res) => {
+  const { userId, classId } = req.query;
+  const existing = await bookClassCollection.findOne({ userId, classId });
+  res.status(200).json({ isBooked: !!existing });
+});
+app.post("/api/bookClass", async (req, res) => {
+  const result = await bookClassCollection.insertOne(req.body);
+  res.status(200).json(result);
+});
+
+//favorite add/remove
+app.post("/api/favorites", async (req, res) => {
+  const { userId, classId } = req.body;
+  const existing = await favoriteCollection.findOne({ userId, classId });
+  if (existing) {
+    await favoriteCollection.deleteOne({ userId, classId });
+    res
+      .status(200)
+      .json({ isFavorite: false, message: "Removed from favorites" });
+  } else {
+    await favoriteCollection.insertOne({
+      ...req.body,
+      createdAt: new Date(),
+    });
+    res.status(200).json({ isFavorite: true, message: "Added to favorites" });
+  }
+});
+
+app.get("/api/favorites/check", async (req, res) => {
+  const { userId, classId } = req.query;
+  const existing = await favoriteCollection.findOne({ userId, classId });
+  res.status(200).json({ isFavorite: !!existing });
+});
+
+// User এর সব favorites
+// app.get("/api/favorites", async (req, res) => {
+//   const { userId } = req.query;
+//   const favorites = await favoritesCollection.find({ userId }).toArray();
+//   res.status(200).json(favorites);
+// });
+
+//trainer related
+app.post("/api/forumPost", async (req, res) => {
+  const post = req.body;
+  const newPost = {
+    ...post,
+    createdAt: new Date(),
+    status: "pending",
+  };
+  const result = await forumPostCollection.insertOne(newPost);
+  res.status(200).json(result);
+});
+
+// Send a ping to confirm a successful connection
+// await client.db("admin").command({ ping: 1 });
+// console.log(
+//   "Pinged your deployment. You successfully connected to MongoDB!",
+// );
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -164,3 +182,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+module.exports = app;
